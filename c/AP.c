132008b5c74e31d206b35d3c85e46adf4a081bed
ShashankShinde16 <ss9575a@gmail.com>
2022-12-09 22:36:27 +0530

    c
    c codes

---
+++ c/AP.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int sin,x ;
+    printf("the values of sinx\n");
+    for (int i = 0; i < 90; i++);
+    {
+       
+    }
+    
+    return 0;
+}
---
+++ c/stringFunctions.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<string.h>
+int main(int argc, char const *argv[])
+{
+    char name1[] = "karan is a friend of";
+    char name2[] = " vivek";
+    puts(strcat(name1,name2));
+    return 0;
+}
